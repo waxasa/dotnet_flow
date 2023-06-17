a61e1f671b70e38db7a986c2138c3c286cf34942
builderchen <builderchen@outlook.com>
2023-06-17 10:43:58 +0800

    Add files

---
+++ dotnet_flow/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace dotnet_flow
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1024;
+            int b = 768;
+            float res = Minus(a, b);
+            Console.WriteLine($"{a} / {b} = {res}");
+        }
+
+        public static float Minus(int a, int b)
+        {
+            return (float)a / b;
+        }
+    }
+}
---
+++ dotnet_flow_Tests/ProgramTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using dotnet_flow;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnet_flow.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void MinusTest()
+        {
+            Assert.AreEqual(15f / 3, dotnet_flow.Program.Minus(15, 3));
+        }
+    }
+}
